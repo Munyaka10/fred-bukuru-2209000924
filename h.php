92e71dc4653cda18d2448f9c6973d5fa13e890db
Munyaka10 <bukurufred@gmail.com>
2025-04-12 20:32:57 +0200

    it's good to in php

---
+++ h.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <?php
+  $phrase="university academy";
+  echo strlower(phrase)# length of phrase
+  echo $phrase [0]"t";
+  echo $pfrase;
+  echo str replase("university","kigali",$pfrase);#this allow us to reprase 
+  echo substr ($phrase, 16,3)
+  ?>  
+</body>
+</html>
---
+++ numberr.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $num;
+    $num=$num+25;
+    $num+=25;
+    echo 40.448;
+    echo 5+9; #you can use any maths symbols
+    echo 10 % 3; #remainder of vision
+    echo 5+9; #modulas
+    echo $num;
+    echo abs (-200)#sbsolute number
+    echo pow (2,4); #power
+    echo sqrt (144);
+    echo max(2,10);# which is bigger
+    echo min(2,10);which number is min
+    echo round(3.7);
+    
+
+
+
+
+    
+    
+    ?>
+    
+</body>
+</html>
